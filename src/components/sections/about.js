8d789278d73f6c3edc182d4c6823a3dadcbf246a
Judy Zhang <judy.j.zhang@gmail.com>
2020-09-24 19:14:27 -0700

    animated profile svg

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import Img from 'gatsby-image';
 import sr from '@utils/sr';
 import { srConfig, github } from '@config';
 import styled from 'styled-components';
